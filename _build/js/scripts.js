8ffd83932f20057526466e56130f37fba6e45ea1
Alex Bouthillier <alex@hencove.com>
2024-07-12 13:25:03 -0400

    form styling commit with button changes

--- _build/js/scripts.js
+++ _build/js/scripts.js
@@ -15,6 +15,11 @@
   document.addEventListener("DOMContentLoaded", function () {
     var buttons = document.querySelectorAll(".think-custom-button");
     buttons.forEach(function (button) {
+      if ($(button).hasClass("wpforms-submit")) {
+        var buttonText = $(button).text();
+        var ButtonTextHTML = '<div class="fusion-button-text">' + buttonText + '</div>';
+        $(button).html(ButtonTextHTML);
+      }
       var OrnamentHTML = '<div class="button__ornament button__ornament--vertical"><div class="button__ornament-top-bottom"></div><div class="button__ornament-left-right"></div></div><div class="button__ornament button__ornament--horizontal"><div class="button__ornament-top-bottom"></div><div class="button__ornament-left-right"></div></div>';
       button.insertAdjacentHTML("afterbegin", OrnamentHTML);
     });
