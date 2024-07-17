35ecc8daafe96c0f032334974c54c7d6ff998035
Alex Bouthillier <alex@hencove.com>
2024-07-17 14:32:27 -0400

    HAV-31 CB1: fixed global styling issue caused by previous commit

--- _build/js/scripts.js
+++ _build/js/scripts.js
@@ -14,18 +14,17 @@
   var container = $(".cb1-hero-banner");
   // var images = $(container).find('.fusion-gallery-image');
   var images = $(container).find('.fusion-image-element span');
-
-  // if (images.count != 0) {
-  // 	// TODO
-  // }
-  var count = 1;
-  $('section#content').attr('data-scroll-container', '');
-  $(container).attr('data-scroll-section', '');
-  $(images).each(function (index) {
-    $(this).attr('data-scroll', '');
-    $(this).attr('data-scroll-speed', count);
-    count++;
-  });
+  if (container.length != 0) {
+    var count = 1;
+    $('.post-content').attr('data-scroll-container', '');
+    $(container).attr('data-scroll-section', '');
+    $(images).each(function (index) {
+      $(this).attr('data-scroll', '');
+      $(this).attr('data-scroll-speed', count);
+      count++;
+    });
+  }
+  ;
 
   //
   //
--- _source/js/_cb1.js
+++ _source/js/_cb1.js
@@ -6,20 +6,18 @@
 	// var images = $(container).find('.fusion-gallery-image');
 	var images = $(container).find('.fusion-image-element span');
 
-	
-	// if (images.count != 0) {
-	// 	// TODO
-	// }
-	var count = 1;
+	if (container.length != 0) {
+		var count = 1;
 
-	$('section#content').attr('data-scroll-container', '');
-	$(container).attr('data-scroll-section', '');
+		$('.post-content').attr('data-scroll-container', '');
+		$(container).attr('data-scroll-section', '');
 
-	$(images).each(function( index ){
-		$(this).attr('data-scroll', '');
-		$(this).attr('data-scroll-speed', count);
-		count++;
-	});
+		$(images).each(function( index ){
+			$(this).attr('data-scroll', '');
+			$(this).attr('data-scroll-speed', count);
+			count++;
+		});
+	};
 
 	//
 	//
