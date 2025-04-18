1d0fb7ce7b1ccd0ca5f2532a7f50faddc9e9cd38
Alana <alana@hencove.com>
2025-04-18 15:40:00 -0400

    an attempt to replicate ornament styling on containers // restyling bio contact form

--- _source/js/scripts.js
+++ _source/js/scripts.js
@@ -12,6 +12,7 @@ import "./pages/ss4w";
 //
 //	? code here will execute before the DOM has fully loaded
 //
+
 (function (document, window, $) {
   //
   //	? code here will execute once the page is ready
@@ -24,4 +25,30 @@ import "./pages/ss4w";
   //
   //
   //
+})(document, window, jQuery);
+
+//* Test script for applying the ornament style on containers //*
+
+(function (document, window, $) {
+  $(window).on("load", function () {
+    var containers = document.querySelectorAll(".ornament-test");
+
+    containers.forEach(function (container) {
+      let verticalOrnament = `
+        <div class="button__ornament button__ornament--vertical">
+          <div class="button__ornament-top-bottom"></div>
+          <div class="button__ornament-left-right"></div>
+        </div>`;
+
+      let horizontalOrnament = `
+        <div class="button__ornament button__ornament--horizontal">
+          <div class="button__ornament-top-bottom"></div>
+          <div class="button__ornament-left-right"></div>
+        </div>`;
+
+      let OrnamentHTML = verticalOrnament + horizontalOrnament;
+
+      container.insertAdjacentHTML("afterbegin", OrnamentHTML);
+    });
+  });
 })(document, window, jQuery);
